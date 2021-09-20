bd1873ea85d2e4aac077c1cd5bfa4d6ed8045acf
Alba Guerrero <al.guerreropuentes@gmail.com>
2021-09-20 23:34:40 +0200

    [X-1] Create base endpoint logic

--- src/main/java/com/xantar/xantarcore/meals/MealsController.java
+++ src/main/java/com/xantar/xantarcore/meals/MealsController.java
@@ -7,10 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.xantar.xantarcore.db.MealsService;
+import com.xantar.xantarcore.models.Meal;
 
 @RestController
 @RequestMapping("/api/meals")
@@ -34,6 +37,14 @@ public class MealsController {
 		return this.mealsService.findAll().stream()
 				.map(meal -> MealResponseMapper.toJsonResponse(meal))
 				.collect(Collectors.toList());
+	}
+
+	@PostMapping()
+	public MealResponseJson createMeal(@RequestBody MealResponseJson jMeal) {
+		final Meal mealModel = MealResponseMapper.toModel(jMeal);
+		final Meal createdMeal = this.mealsService.createMeal(mealModel);
+
+		return MealResponseMapper.toJsonResponse(createdMeal);
 	}
 
 }
