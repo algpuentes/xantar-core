9b4eac7811b4b50685911db9f4316d85e40b6e10
Alba Guerrero <39908737+algpuentes@users.noreply.github.com>
2021-09-25 15:54:56 +0200

    [X-2] add delete meal endpoint

--- src/main/java/com/xantar/xantarcore/meals/MealsController.java
+++ src/main/java/com/xantar/xantarcore/meals/MealsController.java
@@ -8,7 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,4 +53,9 @@ public class MealsController {
 		return new ResponseEntity<MealResponseJson>(responseJMeal, HttpStatus.CREATED);
 	}
 
+	@DeleteMapping("/{mealId}")
+	public ResponseEntity deleteMeal(@PathVariable("mealId") Integer mealId) {
+		this.mealsService.deleteMeal(mealId);
+		return new ResponseEntity(HttpStatus.OK);
+	}
 }
--- src/main/java/com/xantar/xantarcore/rest/RestResponseExceptionHandler.java
+++ src/main/java/com/xantar/xantarcore/rest/RestResponseExceptionHandler.java
@@ -2,6 +2,7 @@ package com.xantar.xantarcore.rest;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +15,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
 	private static final String MESSAGE_DATA_INTEGRITY_VIOLATION = "Data integrity violation.";
+	private static final String MESSAGE_RESOURCE_NOT_FOUND = "Resource not found.";
 
 	@ExceptionHandler(value = { ConstraintViolationException.class, DataIntegrityViolationException.class })
 	protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
 
 		return this.handleExceptionInternal(ex, MESSAGE_DATA_INTEGRITY_VIOLATION,
 				new HttpHeaders(), HttpStatus.CONFLICT, request);
+	}
+
+	@ExceptionHandler(value = { EmptyResultDataAccessException.class })
+	protected ResponseEntity<Object> handleDataAccessError(RuntimeException ex, WebRequest request) {
+
+		return this.handleExceptionInternal(ex, MESSAGE_RESOURCE_NOT_FOUND,
+				new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 }
